e108e371d87b7ef9aa0a0afc0c31b7a4f049ddd7
beten-technologies <beten.technologies@gmail.com>
2016-04-12 22:02:41 +0530

    Inital Release

---
+++ Northspring.Web.Site/Business/Pages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Northspring.Web.Site.Business
+{
+    class Pages
+    {
+        private string defaultPageName = "~/Views/Shared/_pagedown.cshtml";
+        public Pages()
+        {
+
+        }
+
+        public string GetDynamicPageName(string pageNameFromUrl, string controller = "Home", string action = "Pages")
+        {
+            if (string.IsNullOrEmpty(pageNameFromUrl))
+                return defaultPageName;
+
+            string partialViewName = "~/Views/" + controller + "/" + action + "/_" + pageNameFromUrl.ToLower() + ".cshtml";
+
+            return partialViewName;
+        }
+
+        public string GetDefaultPageName()
+        {
+            return defaultPageName;
+        }
+    }
+}
---
+++ Northspring.Web.Site/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Northspring.Web.Site.Business;
+
+namespace Northspring.Web.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pages(string id)
+        {
+            Pages PageCreator = new Pages();
+            string pageName = (id != null) ? id.ToString() : "";
+            string actionName = "Pages";
+
+            if (!string.IsNullOrEmpty(pageName))
+                ViewBag.Title = pageName;
+            else
+                ViewBag.Title = "Page";
+
+            string partialPage = PageCreator.GetDynamicPageName(pageName, action: actionName);
+
+            ViewEngineResult result = ViewEngines.Engines.FindPartialView(ControllerContext, partialPage);
+
+            if (result.View != null)
+                ViewBag.PartialPage = partialPage;
+            else
+                ViewBag.PartialPage = PageCreator.GetDefaultPageName();
+
+            return View();
+        }
+
+        public ActionResult Details(string id)
+        {
+            Pages PageCreator = new Pages();
+            string pageName = (id != null) ? id.ToString() : "";
+            string actionName = "Details";
+
+            if (!string.IsNullOrEmpty(pageName))
+                ViewBag.Title = pageName;
+            else
+                ViewBag.Title = "Page";
+
+            string partialPage = PageCreator.GetDynamicPageName(pageName, action: actionName);
+
+            ViewEngineResult result = ViewEngines.Engines.FindPartialView(ControllerContext, partialPage);
+
+            if (result.View != null)
+                ViewBag.PartialPage = partialPage;
+            else
+                ViewBag.PartialPage = PageCreator.GetDefaultPageName();
+
+            return View();
+        }
+    }
+}
